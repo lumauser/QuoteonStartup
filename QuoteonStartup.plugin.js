1fd490781027bf7a679a003d8129ce7ea88f1a52
Robert Elmore <bobbye23899@hotmail.com>
2024-01-30 17:23:40 +0000

    Authors and quotes dictionary with random selection

--- QuoteonStartup.plugin.js
+++ QuoteonStartup.plugin.js
@@ -2,29 +2,48 @@
  * @name QuoteonStartup
  * @author saulgoodman7277
  * @description Displays a random quote everytime you launch discord.
- * @version 0.0.1
- */
+ * @version 0.0.2
+ **/
 
 module.exports = class QuoteOnStartup {
+    quotes = {
+        "Steve Jobs": [
+            "The only way to do great work is to love what you do.",
+            "Innovation distinguishes between a leader and a follower.",
+        ],
+        Confucius: [
+            "It does not matter how slowly you go as long as you do not stop.",
+        ],
+        "Winston Churchill": [
+            "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        ],
+    };
+
+    static random(range) {
+        return Math.floor(Math.random() * range);
+    }
+
     start() {
         this.displayQuote();
     }
 
-    stop() {
-        
+    stop() {}
+
+    getQuote(author = null) {
+        if (author) {
+            authorQuotes = this.quotes[author];
+            return [author, authorQuotes[random(authorQuotes.length)]];
+        }
+
+        authors = Object.keys(this.quotes);
+        author = authors[random(authors.length)];
+        authorQuotes = this.quotes[randomAuthor];
+
+        return [author, random(authorQuotes.length)];
     }
 
     displayQuote() {
-        const quotes = [
-            "The only way to do great work is to love what you do. – Steve Jobs",
-            "Innovation distinguishes between a leader and a follower. – Steve Jobs",
-            "It does not matter how slowly you go as long as you do not stop. – Confucius",
-            "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
-        ];
-
-        const randomIndex = Math.floor(Math.random() * quotes.length);
-        const quote = quotes[randomIndex];
-
-        BdApi.alert("A Random Quote", quote);
+        [author, quote] = this.getQuote();
+        BdApi.alert(author, quote);
     }
-}
+};
